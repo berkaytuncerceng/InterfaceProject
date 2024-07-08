a7a8713ff1fce2d4d5656dde3df148225f342cef
Berkay Tuncer <berkay.beren11@gmail.com>
2024-07-08 16:50:00 +0300

    Tamamlandı.

--- OOP3/Program.cs
+++ OOP3/Program.cs
@@ -13,13 +13,15 @@ namespace OOP3
             IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
             IKrediManager konutKrediManager = new KonutKrediManager();
             IKrediManager tasitKrediManager = new TasitKrediManager();
-            
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+
             BasvuruManager basvuruManager = new BasvuruManager();
             
-            List<IKrediManager> krediler = new List<IKrediManager>() {konutKrediManager, ihtiyacKrediManager};
-            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            List<IKrediManager> krediler = new List<IKrediManager>() {konutKrediManager, ihtiyacKrediManager , tasitKrediManager};
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
 
-
+            basvuruManager.BasvuruYap(tasitKrediManager, fileLoggerService);
             Console.ReadKey();
         }
     }
