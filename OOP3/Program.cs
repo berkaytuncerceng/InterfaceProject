c78e806f37b1136388f495199396baacb34a8939
Berkay Tuncer <berkay.beren11@gmail.com>
2024-07-08 15:43:06 +0300

    Proje dosyası ekle.

---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            
+            BasvuruManager basvuruManager = new BasvuruManager();
+            
+            List<IKrediManager> krediler = new List<IKrediManager>() {konutKrediManager, ihtiyacKrediManager};
+            basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+            Console.ReadKey();
+        }
+    }
+}
